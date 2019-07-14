fa650f2635de9a9dd370cf6b3da299b4ad07a98f
luchohack12 <franciscorios123456@gmail.com>
2019-07-13 21:54:12 -0600

    navbarcontrol
    hice un form para probarlo

--- Application/TeamCellApplication/TeamCellApplication/Program.cs
+++ Application/TeamCellApplication/TeamCellApplication/Program.cs
@@ -18,7 +18,7 @@ namespace TeamCellApplication
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main.frmMain());
+            Application.Run(new Main.prueba());
         }
     }
 }
